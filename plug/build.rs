77634c0ce0f632c57e1c697d0d4cf94fc1eae765
Garry Red <you@example.com>
2023-06-12 01:52:10 +0200

    Fix automatic header generation

--- plug/build.rs
+++ plug/build.rs
@@ -3,14 +3,7 @@ use std::env;
 extern crate cbindgen;
 
 fn main() {
-    
     cbindgen::generate(env::var("CARGO_MANIFEST_DIR").unwrap())
                 .expect("Unable to generate bindings")
                 .write_to_file("include/telekinesis_plug.h");
-
-    // cbindgen::Builder::new()
-    //    .with_crate(crate_dir)
-    //    .generate()
-    //    .expect("Unable to generate bindings")
-    //    .write_to_file("include/telekinesis_plug.h");
 }
