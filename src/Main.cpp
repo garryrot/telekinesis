48f3273948c7084dbd25527835f916c09f511fda
Garry Red <you@example.com>
2023-06-18 21:15:36 +0200

    Move everything to one file

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,32 +1,44 @@
 #include <stddef.h>
 
-#include "Papyrus.h"
 #include "../plug/include/telekinesis_plug.h"
 #include "../plug/target/cxxbridge/plug/src/logging.rs.h"
 
+using namespace RE;
 using namespace RE::BSScript;
 using namespace SKSE::log;
 using namespace SKSE::stl;
 using namespace SKSE;
-using namespace Telekinesis;
+using namespace REL;
 
-namespace Telekinesis {
-    void InitializePapyrus() {
-        log::trace("Initializing Papyrus binding...");
-        if (GetPapyrusInterface()->Register(Telekinesis::RegisterPapyrusCalls)) {
-            log::debug("Papyrus functions bound.");
-        } else {
-            stl::report_and_fail("Failure to register Papyrus bindings.");
-        }
-    }
+#define DllExport __declspec(dllexport)
 
-    std::string GetLogFile() {
-        auto path = log_directory();
-        if (!path) {
-            report_and_fail("Unable to lookup SKSE logs directory.");
-        }
-        return std::format("{}\\{}.log", path->string(), PluginDeclaration::GetSingleton()->GetName());
+constexpr std::string_view PapyrusClass = "Tele";
+
+bool RegisterPapyrusCalls(IVirtualMachine* vm) {
+    vm->RegisterFunction("ScanForDevices", PapyrusClass, (bool (*)(StaticFunctionTag*))ConnectAndScanForDevices);
+    vm->RegisterFunction("VibrateAll", PapyrusClass, (bool (*)(StaticFunctionTag*, int))VibrateAll);
+    vm->RegisterFunction("VibrateAllFor", PapyrusClass, (bool (*)(StaticFunctionTag*, int, float))VibrateAllFor);
+    vm->RegisterFunction("StopAll", PapyrusClass, (bool (*)(StaticFunctionTag*))StopAll);
+    vm->RegisterFunction("PollEvents", PapyrusClass, (std::vector<std::string>(*)(StaticFunctionTag*))PollEvents);
+    vm->RegisterFunction("Close", PapyrusClass, (bool (*)(StaticFunctionTag*))Close);
+    return true;
+}
+
+void InitializePapyrus() {
+    log::trace("Initializing Papyrus binding...");
+    if (GetPapyrusInterface()->Register(RegisterPapyrusCalls)) {
+        log::debug("Papyrus functions bound.");
+    } else {
+        stl::report_and_fail("Failure to register Papyrus bindings.");
     }
+}
+
+std::string GetLogFile() {
+    auto path = log_directory();
+    if (!path) {
+        report_and_fail("Unable to lookup SKSE logs directory.");
+    }
+    return std::format("{}\\{}.log", path->string(), PluginDeclaration::GetSingleton()->GetName());
 }
 
 SKSEPluginLoad(const LoadInterface* skse) {
--- src/Papyrus.h
+++
@@ -1,6 +0,0 @@
-
-#include <RE/Skyrim.h>
-
-namespace Telekinesis {
-    bool RegisterPapyrusCalls(RE::BSScript::IVirtualMachine* vm);
-}
