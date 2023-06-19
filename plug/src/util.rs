e3e06f457fd5fbae0066bb8ae1200493d47a6aa2
Garry Red <you@example.com>
2023-06-19 21:59:50 +0200

    Generalize narrow function

--- plug/src/util.rs
+++ plug/src/util.rs
@@ -4,13 +4,13 @@ pub trait Narrow<T> {
     fn narrow(self, above: T, below: T) -> T;
 }
 
-impl Narrow<i64> for i64 {
-    fn narrow( self, above: i64, below: i64) -> i64 {
-        match self {
-            a if a < above => above,
-            a if a > below => below,
-            a => a
-        }
-    }
-}    
-
+impl<T: PartialOrd> Narrow<T> for T
+{
+    fn narrow(self, above: T, below: T) -> T {
+         match self {
+             a if a < above => above,
+             a if a > below => below,
+             a => a
+         }
+     }
+}
