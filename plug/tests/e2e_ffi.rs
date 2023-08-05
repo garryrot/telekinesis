9896b4a37f1950841940b082273e6f0f517f74ca
Garry Red <you@example.com>
2023-08-05 12:52:48 +0200

    Adapt e2e test - Manually enables first connected device

--- plug/tests/e2e_ffi.rs
+++ plug/tests/e2e_ffi.rs
@@ -1,12 +1,14 @@
 use telekinesis_plug::*;
-use tracing::Level;
+use tracing::{Level, info, instrument};
 
-use std::{time::Duration, thread};
 use lazy_static::lazy_static;
 use nonparallel::nonparallel;
 use std::sync::Mutex;
+use std::{thread, time::Duration};
 
-lazy_static! { static ref M: Mutex<()> = Mutex::new(()); }
+lazy_static! {
+    static ref M: Mutex<()> = Mutex::new(());
+}
 
 #[allow(dead_code)]
 fn enable_log() {
@@ -18,28 +20,42 @@ fn enable_log() {
     .unwrap();
 }
 
-// These tests asserts that exactly one vibrating device is connected
-// and will not work without actual hardwarce connected via usb
 #[test]
+#[ignore = "Requires vibrator to be connected via BTLE (vibrates it)"]
 #[nonparallel(M)]
 fn ffi_test_reconnect() {
-    _ffi_test_event_polling();
-    _ffi_test_event_polling();
+    enable_log();
+    test_vibration_e2e();
+    thread::sleep(Duration::from_secs(5));
+    test_vibration_e2e();
 }
 
-fn _ffi_test_event_polling() {
+#[instrument]
+fn test_vibration_e2e() {
+    // arrange
     tk_connect();
     tk_scan_for_devices();
-    thread::sleep(Duration::from_secs(5));
-    tk_vibrate_all(100);
+    wait_for_device_connect(Duration::from_secs(5));
+
+    // act
+    tk_vibrate(100, 1);
     thread::sleep(Duration::from_millis(200));
     tk_stop_all();
     thread::sleep(Duration::from_millis(200));
+
+    // assert
     let events = tk_poll_events();
-    assert!(events[0].starts_with("Device"));
-    assert!(events[1].starts_with("Vibrating"));
-    assert!(events[2].starts_with("Stopping"));
+    assert!(events[0].starts_with("Vibrating"));
+    assert!(events[1].starts_with("Stopping"));
     tk_close();
-    thread::sleep(Duration::from_secs(5));
 }
- 
+
+fn wait_for_device_connect(duration: Duration) {
+    thread::sleep(duration);
+    let events = tk_poll_events();
+    let mut split = events[0].split("'");
+    assert!( split.next().unwrap().starts_with("Device") );
+    let device = split.next().unwrap();
+    info!("Enabling device '{}'", device);
+    tk_settings_set_enabled(device, true);
+}
