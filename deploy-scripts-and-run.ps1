505f60434db61f719d875381a6ddcaedc95a3cec
Garry Red <you@example.com>
2023-09-08 02:52:25 +0200

    Deploy patterns

--- deploy-scripts-and-run.ps1
+++ deploy-scripts-and-run.ps1
@@ -1,2 +1,3 @@
 cp -v ".\contrib\Distribution\SKSE\Plugins\*.dll"  "C:\Program Files (x86)\Steam\steamapps\common\Skyrim Special Edition\Data\SKSE\Plugins\"
 cp -v ".\contrib\Distribution\Scripts\*.pex" "C:\Program Files (x86)\Steam\steamapps\common\Skyrim Special Edition\Data\Scripts\";
+cp -v ".\contrib\Distribution\SKSE\Plugins\Telekinesis\Patterns\*.funscript"  "C:\Program Files (x86)\Steam\steamapps\common\Skyrim Special Edition\Data\SKSE\Plugins\Telekinesis\Patterns\"
